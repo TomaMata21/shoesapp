5341652ba188bae077160ea256cc915f8b9c4355
volundir <programmerjohn2019@gmail.com>
2024-01-08 09:51:42 +0100

    init configs files

--- lib/configs/app_configs.dart
+++ lib/configs/app_configs.dart
@@ -1,4 +1,9 @@
 import 'dart:math';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:shoesap/configs/assets.dart';
+import 'package:shoesap/model/shoe.dart';
 
 class AppConfigs {
   static bool getRandomBool() {
@@ -6,4 +11,46 @@ class AppConfigs {
     return random.nextBool();
   }
 
+  static String getRandomName() {
+    List<String> titles = [
+      "AIR JORDAN SLANEY JSP",
+      "AIR-270",
+      "HUSTLED BY A SHOE",
+      "NIKE AIR MAX",
+    ];
+    Random random = Random();
+    int randomIndex = random.nextInt(titles.length);
+
+    return titles[randomIndex];
+  }
+
+  static int getRandomPrice() {
+    Random random = Random();
+    int basePrice = 100;
+    int priceRange = 3750 - basePrice;
+    int numberOfSteps = priceRange ~/ 50;
+
+    int randomStep = random.nextInt(numberOfSteps + 1);
+    int totalPrice = basePrice + randomStep * 50;
+
+    return totalPrice;
+  }
+
+  static Color getRandomAccentColor() =>
+      Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+
+  static List<Shoe> getShoes() {
+    return List.generate(
+      12,
+      (index) => Shoe(
+        name: getRandomName(),
+        image: Assets.images(index + 1),
+        brand: "Nike",
+        description: "description",
+        price: "\$${getRandomPrice()}",
+        isNew: getRandomBool(),
+        quantity: index + 1,
+      ),
+    );
+  }
 }
